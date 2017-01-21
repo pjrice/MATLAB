c3bb494cce7c26c0d31b479e69318c15ccd9423d
pjrice <pjrice@uw.edu>
2017-01-21 15:52:20 -0800

    more ANOVA work v2

--- MATLAB/DRI/TMS/analysis/RR_TMS_analysis/RR_TMS_anovas2.m
+++ MATLAB/DRI/TMS/analysis/RR_TMS_analysis/RR_TMS_anovas2.m
@@ -6,19 +6,20 @@
 stimIdx = find(dtable.success==1 &...
     dtable.eslsns_trials~=2);
 
-
+%make data vector
 y = dtable.stimRT(stimIdx);
 
+%make condition indices
 SF = dtable.sf_trials(stimIdx);
 PV = dtable.pmdver_trials(stimIdx);
 EL = dtable.eslsns_trials(stimIdx);
 InfIns = dtable.infins_trials(stimIdx);
-subj = dtable.subj_ids(stimIdx);
 
+%run ANOVA
 [~,~,stats] = anovan(y,{SF PV EL InfIns},'model','full',...
     'varnames',{'SF','PV','EL','InfIns'});
 
-
+%do multiple comparisons
 figure(1)
 results = multcompare(stats,'Dimension',[1]);
 figure(2)
@@ -55,22 +56,78 @@ figure(15)
 results = multcompare(stats,'Dimension',[1 2 3 4]);
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Test ALL Vertex stim trials against ALL no stim trials
+nsVsidx = find(dtable.success==1 &... %successes and...
+    dtable.pmdver_trials==1 |... %vertex trials or...
+    dtable.eslsns_trials==2); %all no stim trials
+
+y = dtable.stimRT(nsVsidx);
+
+ELN = dtable.eslsns_trials(nsVsidx);
+
+[~,~,stats] = anovan(y,{ELN},'model','full',...
+    'varnames',{'ELN'});
+
+multcompare(stats,'Dimension',[1])
+
+%this shows that both early vertex and late vertex stimulation trials are
+%not different than PMd/Vertex no stimulation trials
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%the following is probably fucked because I was haphazardly testing
+
+%test no stim against vertex stim
+
+%get trials to test
+nsVs_idx = find(dtable.success==1 &... %successes and...
+    (dtable.eslsns_trials==2 |... %no stim trials or...
+    (dtable.pmdver_trials==1 & dtable.eslsns_trials~=2))); %vertex stim trials
+
+%make data vector
+y = dtable.stimRT(nsVs_idx);
+
+%make "stimmed or not stimmed" vector
+nsVs = dtable.eslsns_trials(nsVs_idx);
+% sIdx = find(nsVs~=2);
+% nsIdx = find(nsVs==2);
+% nsVs(sIdx) = 1; %1 indexes Vstim trials
+% nsVs(nsIdx) = 0; %0 indexes nostim trials
+
+%make other condition indices
+SF = dtable.sf_trials(nsVs_idx);
+InfIns = dtable.infins_trials(nsVs_idx);
 
 
+% [~,~,stats] = anovan(y,{nsVs},'model','full',...
+%     'varnames',{'nsVs'});
+nsVs_idx = find(dtable.success==1 &... %successes and...
+    (dtable.eslsns_trials==2 |... %no stim trials or...
+    (dtable.pmdver_trials==1 & dtable.eslsns_trials~=2))); %vertex stim trial
+
+%make data vector
+y = dtable.stimRT(nsVs_idx);
+
+SF = dtable.sf_trials(nsVs_idx);
+PV = dtable.pmdver_trials(nsVs_idx);
+ELN = dtable.eslsns_trials(nsVs_idx);
+InfIns = dtable.infins_trials(nsVs_idx);
+
+[~,~,stats] = anovan(y,{SF PV ELN InfIns},'model','full',...
+    'varnames',{'SF','PV','EL','InfIns'});
+[~,~,stats] = anovan(y,{SF nsVs InfIns},'model','full',...
+    'varnames',{'SF','nsVs','InfIns'});
+
+multcompare(stats,'Dimension',[2])
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
-
-
-
-
-
-
-
+%in "actual" analysis (aka comparing PMd trials to something else), replace
+%Vertex stim trials with PMd no stim trials and see what's up
 
 
 
